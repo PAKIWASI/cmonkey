082a94e55be1ba9d08071185be8e55c869a25e46
PAKIWASI <ullahwasi26@gmail.com>
2026-04-26 17:25:17 +0500

    cmonkey struct

--- include/cmonkey.h
+++ include/cmonkey.h
@@ -2,15 +2,28 @@
 #define CMONKEY_H
 
 #include "buffer.h"
+#include "config.h"
 #include "wordbank.h"
 
 
 typedef struct {
     WordBank* wb;
     term_buf* b;
-
+    cmonkey_theme* t;
+    cmonkey_conf*  c;
+    u32 rows;
+    u32 cols;
+    bool resize;
+    bool quit;
 } cmonkey;
 
+void cmonkey_begin(cmonkey* cm, const char* wb_path, const char* theme_path, const char* conf_path);
+
+void cmonekey_end(cmonkey* cm);
+
+void cmonkey_update(cmonkey* cm);
+
+void cmonkey_draw(cmonkey* cm);
 
 
 
