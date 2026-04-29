2dce1ebaad796f8015e2b9057c722b89dcc4f903
PAKIWASI <ullahwasi26@gmail.com>
2026-04-29 22:00:43 +0500

    cmonkey struct

--- include/cmonkey.h
+++ include/cmonkey.h
@@ -1,20 +1,22 @@
 #ifndef CMONKEY_H
 #define CMONKEY_H
 
+#include "wordbank.h"
+#include "Queue_single.h"
 #include "buffer.h"
 #include "config.h"
-#include "wordbank.h"
 
 
 typedef struct {
-    WordBank* wb;
-    term_buf* b;
-    cmonkey_theme* t;
-    cmonkey_conf*  c;
-    u32 rows;
-    u32 cols;
-    bool resize;
-    bool quit;
+    WordBank*       wb;
+    Queue*          q;
+    term_buf*       b;
+    cmonkey_theme*  t;
+    cmonkey_conf*   c;
+    u32             rows;
+    u32             cols;
+    bool            resize;
+    bool            quit;
 } cmonkey;
 
 void cmonkey_begin(cmonkey* cm, const char* wb_path, const char* theme_path, const char* conf_path);
--- src/cmonkey.c
+++ src/cmonkey.c
@@ -1,2 +1,8 @@
 #include "cmonkey.h"
 
+
+
+
+void cmonkey_begin(cmonkey* cm, const char* wb_path, const char* theme_path, const char* conf_path)
+{
+}
